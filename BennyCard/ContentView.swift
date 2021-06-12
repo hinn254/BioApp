3d4d5b9537cef05982ddd66bc033a8046e04a865
hinn254 <bennyhinnotieno@gmail.com>
2021-06-12 19:24:54 +0300

    basic layout

--- BennyCard/ContentView.swift
+++ BennyCard/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Benny").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150)
+                Text("Benny Otieno")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+
+            }
+        }
+        
     }
 }
 
