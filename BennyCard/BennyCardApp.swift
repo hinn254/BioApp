25f364edc0507c826e2f092575633c5543b064f1
hinn254 <bennyhinnotieno@gmail.com>
2021-06-12 18:31:11 +0300

    Initial Commit

---
+++ BennyCard/BennyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BennyCardApp.swift
+//  BennyCard
+//
+//  Created by Benny Hinn on 12/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BennyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
